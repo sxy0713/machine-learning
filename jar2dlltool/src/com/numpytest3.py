d4dc6da368d64deab8549b6709f10fec3826dcae
Jun.Shen <sxy0713@163.com>
2019-02-01 10:14:43 +0800

    Add the numpy test3.

--- jar2dlltool/src/com/numpytest3.py
+++ jar2dlltool/src/com/numpytest3.py
@@ -46,4 +46,26 @@ if __name__ == '__main__':
     print  ('以 F 风格顺序排序：')  
     for x in np.nditer(a, order = 'F'):  
         print (x,)
+    
+    #修改数组的值
+    #nditer对象有另一个可选参数op_flags。 其默认值为只读，但可以设置为读写或只写模式。 这将允许使用此迭代器修改数组元素。
+    for x in np.nditer(a, op_flags=['readwrite']): 
+        x[...] = 2 * x 
+    print ('修改后的数组是：')  
+    print (a) 
 
+    #迭代器遍历对应于每列的一维数组
+    print  ('\n')  
+    print  ('迭代器遍历对应于每列的一维数组：')  
+    for x in np.nditer(a, flags = ['external_loop'], order = 'F'):  
+        print (x,)
+    
+    #如果两个数组是可广播的, 数组b被广播到a的大小
+    print  ('\n')  
+    print  ('第二个数组：') 
+    b = np.array([1,  2,  3,  4], dtype =  int)  
+    print (b) 
+    print  ('\n')  
+    print  ('修改后的数组是：')  
+    for x,y in np.nditer([a,b]):  
+        print  ("%d:%d"  %  (x,y),)
